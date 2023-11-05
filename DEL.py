8d15729d4bd71ad5f71517a082d9b3201bde793d
mpwwww <111195758+mpwwww@users.noreply.github.com>
2023-11-05 13:40:10 -0500

    github action

--- DEL.py
+++ DEL.py
@@ -3,7 +3,6 @@ delte misskey notes posted a month ago
 """
 from misskey import Misskey
 import os
-import json
 import datetime
 import time
 
@@ -16,16 +15,20 @@ mk = Misskey(domain, i=token)
 # get user id
 my_id = mk.i()["id"]
 print('my id: ' , my_id)
-status = mk.users_notes(user_id=my_id, until_date=datetime.datetime.now() - datetime.timedelta(days=120), limit=100)    
+  
 
-# beautify the status json and print out
-print('status:')
-print(status)
+
+# print('status:')
+# print(status)
 
 # delete notes from the status dict
 
 while True:
-     # wait for 30 seconds
+    status = mk.users_notes(user_id=my_id, until_date=datetime.datetime.now() - datetime.timedelta(days=120), limit=100)  
+    if  status == []:
+        print('no notes to delete')
+        break
+    time.sleep(12)
     
     for note in status:
         try:
@@ -36,14 +39,6 @@ while True:
             print(e)
             time.sleep(12)
             continue
-    try:  
-        stts = status
-        time.sleep(12)
-        
-    except Exception as e:
-        print(e)
-        continue
+
     
-    if [] + stts == []:
-        print('no notes to delete')
-        break
+    
